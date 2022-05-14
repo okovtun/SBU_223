50193fbbf0d09ec2ae28dbc39c51dfa5fece658c
Oleg <31523835+okovtun@users.noreply.github.com>
2022-05-14 14:37:21 +0300

    DataTypes day 1

---
+++ DataTypes/DataTypes/main.cpp
@@ -0,0 +1,23 @@
+//DataTypes
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	//cout << "Привет!";
+	//ОЗУ (RAM)
+	//endl - end line ("\n")
+	/*cout << true << endl;	
+	cout << false << endl;
+	cout << "\n";
+	cout << endl;
+	cout << (bool)"" << endl;
+	cout << (bool)'\0' << endl;
+	cout << '0' << endl;
+	bool is_day = false;
+	cout << is_day << endl;*/
+
+	cout << sizeof(int) << endl;
+	cout << INT_MIN << "\t" << INT_MAX << endl;
+}
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,3 +1,8 @@
 https://github.com/okovtun/SBU_223.git
 https://www.youtube.com/watch?v=YQN-DRgrplc&list=PLeqyOOqxeiIMvjJfSBYoAXa7Kq8V7I2HV
 https://meet.google.com/mho-khdw-fhj
+
+TODO:
+1. ¬ проекте Elochka использу€ Escape-последовательности вывести в консоль
+   стишок "¬ лесу родилась ®лочка". —тишок должен быть выровнен по середине
+   окна консоли и его куплеты должны быть разделены пустой строкой.
