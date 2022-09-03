caa5eda894601769274b1e5bee37c3640d6a14dc
Clayman <Clayman@DESKTOP-DM8E77U>
2022-09-03 14:41:20 +0300

    Functions day 1

---
+++ Functions/Arrays/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand()%100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
--- Functions/Functions/main.cpp
+++ Functions/Functions/main.cpp
@@ -1,8 +1,46 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
+
+//Coffee CoffeeMachine(double money);
+
+int Add(int a = 0, int b = 0);	//Прототип функции - Объявление функции (Function declaration)
+int Sub(int a, int b);
+int Mul(int a, int b);
+double Div(int a, int b);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello Functions" << endl;
+	//cout << "Hello Functions" << endl;
+	int a, b;
+	cout << "Введите два числа: "; cin >> a >> b;
+
+	int c = Add(a, b);	//Использование функции - Вызов функции (Function call)
+
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Sub(a, b) << endl;
+	cout << a << " * " << b << " = " << Mul(a, b) << endl;
+	cout << a << " / " << b << " = " << Div(a, b) << endl;
+}
+
+int Add(int a, int b)		//Реализация функции - Определение функции (Function definition)
+{
+	//Addition - сложение
+	int c = a + b;
+	return c;
+}
+int Sub(int a, int b)
+{
+	//Subtraction - вычитание
+	return a - b;
+}
+int Mul(int a, int b)
+{
+	//Multiplication - умножение
+	return a * b;
+}
+double Div(int a, int b)
+{
+	//Division - деление
+	return (double)a / b;
 }
